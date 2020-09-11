8539ad5d840c09db8e98eb115a53c2d31fc2f497
tarmes <traarmes@gmail.com>
2020-09-11 11:27:41 -0400

    axios request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +9,28 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [characters, setCharacters] = useState([])
+
+  const [currentFriendId, setCurrentFriendId] = useState('1')
+
+  const openDetails = id => {
+    setCurrentFriendId(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentFriendId(null)
+  }
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        console.log(res.data.results)
+      })
+      .catch(err => {
+        debugger
+      })
+  })
 
   return (
     <div className="App">
